39647043218b0f663269812d35d87c5f7479c0d4
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-01-30 18:29:00 +0000

    Refactor api to return data points instead

--- backend/api_handler.py
+++ backend/api_handler.py
@@ -3,61 +3,71 @@ from pose_estimator import PoseNetDetector
 import numpy as np
 import cv2
 import struct
+import pickle
 
-
-pose_detector = PoseNetDetector()
 
 def convert_data_to_cv2image(image_data):
     nparr = np.frombuffer(image_data, np.uint8)
-    opencv_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    return opencv_img
 
 def listen_for_data():
     # Create a socket object
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to a specific address and port
     PORT = 8888
-    serversocket.bind(("localhost", PORT))
+    server_socket.bind(("localhost", PORT))
 
     # Start listening for incoming connections
-    serversocket.listen(5)
+    server_socket.listen(5)
+    server_socket.settimeout(1.0)
 
     print(f"Listening on port {PORT}...")
 
     while True:
-        # Establish a connection with a client
-        (clientsocket, address) = serversocket.accept()
+        # Establish a connection with a client (while allowing keyboard interrupts)
+        try:
+            (client_socket, address) = server_socket.accept()
+        except socket.timeout as e:
+            continue
 
-        # Recieve the number of frames
-        num_frames_bytes = clientsocket.recv(4)
+        print('Connection accepted...')
+
+        # Receive the number of frames
+        num_frames_bytes = client_socket.recv(4)
         num_frames = struct.unpack("!i", num_frames_bytes)[0]
         if num_frames == -1:
             print('Video feed detected. Limiting to 10000 frames...')
             num_frames = 10000
 
-        print(f'Receiving {num_frames} frames...')
+        # print(f'Receiving {num_frames} frames...')
 
         for i in range(num_frames):
             # Receive data from the client
-            data = clientsocket.recv(1000000)
+            data = client_socket.recv(1000000)
             image = convert_data_to_cv2image(data)
+            print(f'Received {i + 1}/{num_frames} frames...', end='\r')
 
             # Process the data
-            output = pose_detector.make_prediction(image)
-            success, image_data = cv2.imencode('.jpg', output)
+            key_points_pred = pose_detector.make_prediction(image)
+            client_socket.send(pickle.dumps(key_points_pred))
+            
 
-            if success:
+
+            # success, image_data = cv2.imencode('.jpg', output)
+
+            # if success:
                 # Send the output back to the client
-                clientsocket.send(image_data.tobytes())
-            else:
-                raise ValueError('Error encoding image')
-
+            #     client_socket.send(image_data.tobytes())
+            # else:
+            #     raise ValueError('Error encoding image')
 
         # Close the connection with the client
-        clientsocket.close()
+        print('Done. Listening...')
+        client_socket.close()
 
 
 if __name__ == '__main__':
+    pose_detector = PoseNetDetector(1)
     listen_for_data()
--- backend/api_handler_test.py
+++ backend/api_handler_test.py
@@ -1,3 +1,4 @@
+import pickle
 import socket
 import cv2
 import numpy as np
@@ -6,12 +7,12 @@ import struct
 
 def convert_data_to_cv2image(image_data, willSave):
     nparr = np.frombuffer(image_data, np.uint8)
-    opencv_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    open_cv_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     if willSave:
-        cv2.imwrite("backend/assets/processedImage.jpg", opencv_img)
+        cv2.imwrite("backend/assets/processedImage.jpg", open_cv_img)
 
-    return opencv_img
+    return open_cv_img
 
 
 def send_image(image_path):
@@ -20,36 +21,37 @@ def send_image(image_path):
         image_data = image_file.read()
 
     # Create a socket object
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect to the server
-    clientsocket.connect(("localhost", 8888))
+    client_socket.connect(("localhost", 8888))
 
     # Send the image data to the server
-    clientsocket.sendall(image_data)
+    client_socket.sendall(image_data)
 
     # Receive the output from the server
-    output = clientsocket.recv(1000000)
+    output = client_socket.recv(1000000)
 
     # Close the connection with the server
-    clientsocket.close()
+    client_socket.close()
 
     convert_data_to_cv2image(output, True)
 
 
 def send_video(video_path):
     # Open the video file
+    # video = cv2.VideoCapture(0)
     video = cv2.VideoCapture(video_path)
     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # Create a socket object
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect(("localhost", 8888))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("localhost", 8888))
 
-    # Send and recieve the data
+    # Send and receive the data
     print(f'Sending {total_frames} frames...')
     num_frames_bytes = struct.pack("!i", total_frames)
-    clientsocket.sendall(num_frames_bytes)
+    client_socket.sendall(num_frames_bytes)
 
     frames = []
     while True:
@@ -57,30 +59,79 @@ def send_video(video_path):
         if not ret:
             break
         _, img_encoded = cv2.imencode('.jpg', frame)
-        clientsocket.sendall(img_encoded.tobytes())
-        
-        frames_data = clientsocket.recv(1000000)
-        frame = convert_data_to_cv2image(frames_data, True)
+        client_socket.sendall(img_encoded.tobytes())
 
-        if total_frames != -1:
-            frames.append(frame)
-        else:
-            cv2.imshow("Processed frame", frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        data = client_socket.recv(1000)
+        key_point_pred = pickle.loads(data)
 
-    print(f"Recieved {len(frames)} frames back.")
+        draw_key_points(frame, key_point_pred, 0.4)
+        draw_connections(frame, key_point_pred, 0.4, EDGES)
+
+        cv2.imshow("Processed frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        frames.append(frame)
+
+
+    print(f"Received {len(frames)} frames back.")
     # Close the connection with the server
-    clientsocket.close()
+    client_socket.close()
     video.release()
 
     for frame in frames:
         cv2.imshow("frame", frame)
-        cv2.waitKey(10) # doesnt account for frame rate too well
+        cv2.waitKey(10) # doesn't account for frame rate too well
+
+
+def draw_key_points(p_frame, key_points, confidence_threshold):
+    y, x, c = p_frame.shape
+    shaped = np.squeeze(np.multiply(key_points, [y, x, 1]))
+
+    for kp in shaped:
+        ky, kx, kp_conf = kp
+        if kp_conf > confidence_threshold:
+            cv2.circle(p_frame, (int(kx), int(ky)), 4, (0, 255, 0), -1)
+
+
+def draw_connections(p_frame, key_points, confidence_threshold, EDGES):
+    y, x, c = p_frame.shape
+    shaped = np.squeeze(np.multiply(key_points, [y, x, 1]))
+
+    for edge, color in EDGES.items():
+        p1, p2 = edge
+        y1, x1, c1 = shaped[p1]
+        y2, x2, c2 = shaped[p2]
+
+        if (c1 > confidence_threshold) & (c2 > confidence_threshold):
+            cv2.line(p_frame, (int(x1), int(y1)),
+                    (int(x2), int(y2)), (0, 0, 255), 2)
 
 
 if __name__ == '__main__':
     image_path = "backend/assets/pose_test_image.png"
     video_path = "backend/assets/pose_test_video.mp4"
+    squat_video_path = "backend/assets/squat_test_video.mp4"
     # send_image(image_path)
-    send_video(video_path)
+    # send_video(video_path)
+    EDGES = {
+        (0, 1): 'm',
+        (0, 2): 'c',
+        (1, 3): 'm',
+        (2, 4): 'c',
+        (0, 5): 'm',
+        (0, 6): 'c',
+        (5, 7): 'm',
+        (7, 9): 'm',
+        (6, 8): 'c',
+        (8, 10): 'c',
+        (5, 6): 'y',
+        (5, 11): 'm',
+        (6, 12): 'c',
+        (11, 12): 'y',
+        (11, 13): 'm',
+        (13, 15): 'm',
+        (12, 14): 'c',
+        (14, 16): 'c'
+    }
+    send_video(squat_video_path)
