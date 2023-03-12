930c667335d8aa89c370dd2f8ff8f7cbe11939a3
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-03-12 09:29:51 +0000

    Deletions and changes

--- frontend/src/components/LiveAnalysis_RTMPStream.tsx
+++ frontend/src/components/LiveAnalysis_RTMPStream.tsx
@@ -1,86 +1,20 @@
-
-import React, { useState, useEffect, useRef } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { NodeCameraView } from 'react-native-nodemediaclient';
+import React, { useRef } from 'react';
+import RTMPPublisher, { RTMPPublisherRefProps } from 'react-native-rtmp-publisher';
 
 
-export default function CameraStream() {
-  // const [hasPermission, setHasPermission] = useState(null);
-  const clientRef = useRef<NodeCameraView>(null);
-
-
-
-  const startStreaming = () => {
-    clientRef.current.start();
-  };
-
-  const stopStreaming = () => {
-    clientRef.current.stop();
-  };
-
-  // if (hasPermission === null) {
-  //   return <View />;
-  // }
-
-  // if (hasPermission === false) {
-  //   return <Text>No access to camera</Text>;
-  // }
+export default function CameraStreamingComponent() {
+  const publisherRef = useRef<RTMPPublisherRefProps>();
 
   return (
-    <View style={styles.container}>
-      <NodeCameraView 
-        style={{ height: 400 }}
-        ref={(vb) => {this.vb = vb}}
-        outputUrl = {"rtmp://192.168.0.28:5000/live/stream"}
-        camera={{ cameraId: 1, cameraFrontMirror: true }}
-        audio={{ bitrate: 32000, profile: 1, samplerate: 44100 }}
-        video={{ preset: 12, bitrate: 400000, profile: 1, fps: 15, videoFrontMirror: false }}
-        autopreview={true}
-      >
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-          >
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={startStreaming}
-          >
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Start </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={stopStreaming}
-          >
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Stop </Text>
-          </TouchableOpacity>
-        </View>
-      </NodeCameraView>
-    </View>
+    <RTMPPublisher
+      ref={publisherRef}
+      streamURL="rtmp://your-publish-url"
+      streamName="stream-name"
+      onConnectionFailed={(error) => console.warn(error)}
+      onConnectionStarted={() => console.log('Connection Started')}
+      onConnectionSuccess={() => console.log('Connection Success')}
+      onDisconnect={() => console.log('Disconnected')}
+      onNewBitrateReceived={(newBitRate) => console.log('New Bitrate Received: ' + newBitRate)}
+    />
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+}
