2595a4be5af554f777bfb13ee19a4b09233c863b
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-03-27 20:23:11 +0100

    Re-factor ffmpef into server script

--- backend/rtmp_server.py
+++ backend/rtmp_server.py
@@ -3,21 +3,15 @@ import cv2
 import socket
 
 
-# ip = socket.gethostbyname(socket.gethostname())
-# video_stream_input = f'rtmp://{ip}:1935/form_analyser/22022001'
+ip = socket.gethostbyname(socket.gethostname())
+video_stream_input = f'rtmp://{ip}:1935/form_analyser/22022001'
 local_rtmp_endpoint = 'rtmp://localhost/live'
 
-# THIS COMMAND WORKS!!!!!!!!
-# ffmpeg -re -f flv -listen 1 -i rtmp://192.168.0.28:1935/form_analyser/22022001 -c copy -f flv -listen 1 rtmp://localhost/live
+ffmpeg_cmd = f'ffmpeg -re -f flv -listen 1 -i {video_stream_input} -c copy -f flv -listen 1 {local_rtmp_endpoint}'
+proc1 = subprocess.Popen(ffmpeg_cmd, shell=True)
 
-# This command works to save the video:
-# ffmpeg -re -f flv -listen 1 -i rtmp://192.168.0.28:1935/form_analyser/22022001 -c copy -f flv output.flv
-
-# ffmpeg_cmd = f'ffmpeg -re -f flv -listen 1 -i {video_stream_input} -c copy -f flv {local_rtmp_endpoint}'
-# proc1 = subprocess.Popen(ffmpeg_cmd, shell=True)
-
-cap = cv2.VideoCapture(local_rtmp_endpoint)
-# cap.open(local_rtmp_endpoint)
+cap = cv2.VideoCapture()
+cap.open(local_rtmp_endpoint)
 
 if not cap.isOpened():
     print('Stream not open')
