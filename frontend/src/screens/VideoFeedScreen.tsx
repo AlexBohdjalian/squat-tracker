5c65eac6df0de65993637927df2a4835afd8d993
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-04-04 21:00:26 +0100

    Delete old code

--- frontend/src/screens/VideoFeedScreen.tsx
+++ frontend/src/screens/VideoFeedScreen.tsx
@@ -1,54 +1,9 @@
 import { StyleSheet } from 'react-native';
-import { View } from '../components/Themed';
-import { useState } from 'react';
-import CameraButton from '../components/CameraButton';
-import LocalCamera from '../components/LocalCamera';
-import Colors from '../../constants/Colors';
-import useColorScheme from '../../hooks/useColorScheme';
 import { RootTabScreenProps } from '../../types';
-// import PoseCameraRNVC from '../components/LiveAnalysis_RNVisionCamera';
-// import PoseCameraExpo from '../components/LiveAnalysis_ExpoCamera';
 import PoseCameraRTMP from '../components/LiveAnalysis_RTMPStream';
-// import PoseCameraNMC from '../components/LiveAnalysis_NodeMediaClient';
 
 export default function VideoFeedScreen({ navigation }: RootTabScreenProps<'VideoFeed'>) {
   return <PoseCameraRTMP />;
-  
-  const [trackingForm, setTrackingForm] = useState(false);
-  const scheme = useColorScheme();
-
-  if (trackingForm) {
-    return (
-      <View style={styles.container}>
-        {/* <PoseCameraRNVC /> */}
-        {/* <PoseCameraExpo /> */}
-        <PoseCameraRTMP />
-        {/* <PoseCameraNMC /> */}
-        <View style={styles.trackingToggle}>
-          <CameraButton
-            title={'Stop Tracking'}
-            icon="stop"
-            color={Colors[scheme].text}
-            onPress={() => setTrackingForm(false)}
-          />
-        </View>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <LocalCamera />
-        <View style={styles.trackingToggle}>
-          <CameraButton
-            title={'Begin Tracking'}
-            icon="play"
-            color={Colors[scheme].text}
-            onPress={() => setTrackingForm(true)}
-          />
-        </View>
-      </View>
-    );
-  }
 }
 
 const styles = StyleSheet.create({
